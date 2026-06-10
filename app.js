86199f6c435123261e7671c9806a0b067a6aad9a
ser-gios <pensarnocreer@gmail.com>
2026-06-10 11:49:37 -0600

    Soportar enlaces de tipo YouTube Shorts en getYoutubeVideoId

--- app.js
+++ app.js
@@ -12712,7 +12712,7 @@ function filterTutorials() {
  */
 function getYoutubeVideoId(url) {
     if (!url) return null;
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|shorts\/)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
 }
